6abcd8c33ed25d6b35cf1f8926207c1e9aa395e6
Brad Beattie <bradbeattie@gmail.com>
2009-03-07 11:08:14 -0800

    Fixing old references

--- init.rb
+++ init.rb
@@ -1,6 +1,6 @@
 require 'redmine'
 
-Redmine::Plugin.register :redmine_schedules do
+Redmine::Plugin.register :redmine_my_widgets do
 	name 'Redmine My Widgets plugin'
 	author 'Brad Beattie'
 	description 'This plugin provides instances of Redmine with additional widgets for My page.'

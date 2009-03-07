a344406268d2886d749f8d7af5db30d8ec34f12c
Brad Beattie <bradbeattie@gmail.com>
2009-03-07 10:57:08 -0800

    First commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_schedules do
+	name 'Redmine My Widgets plugin'
+	author 'Brad Beattie'
+	description 'This plugin provides instances of Redmine with additional widgets for My page.'
+	version '0.1.0'
+end
